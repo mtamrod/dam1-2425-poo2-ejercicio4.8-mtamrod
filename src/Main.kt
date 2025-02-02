5c9b598b533a9170ca3505611459da27f8b62f56
mtamrod <trmarioiesrafaelalberti.es>
2025-02-02 22:55:32 +0100

    Minor Fix

--- src/Main.kt
+++ src/Main.kt
@@ -2,7 +2,6 @@ fun main() {
 
     val libro1 = Libro("Harry Potter y la Piedra filosofal", "J.K.Rowling", 130, 10)
     val libro2 = Libro("Harry Potter y la Camara de los Secretos", "J.K.Rowling", 156, 10)
-
     val conjunto1 = conjuntoLibros()
 
     conjunto1.anadirLibro(libro1)
